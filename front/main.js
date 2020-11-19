0b1f9ac6f0d1fc96a27f4ea1cbb0d9c3b7c5b313
Juan José Blanco <juan.blanco03@uptc.edu.co>
2020-11-19 02:55:39 -0500

    Actualización de archivos de Frontend

--- front/main.js
+++ front/main.js
@@ -1,10 +1,38 @@
 let input = document.getElementsByClassName('input');
 for (let index = 0; index < input.length; index++) {
-    input[index].addEventListener("keyup", function(){
-        if(this.value.length >= 1){
-            this.nextElementSibling.classList.add('set');            
+    input[index].addEventListener("keyup", function () {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('set');
         } else {
-            this.nextElementSibling.classList.remove('set');            
+            this.nextElementSibling.classList.remove('set');
         }
-    });    
+    });
+}
+
+$(document).ready(() => {
+    $("#buttonJquery").click(getPeople);
+    getPeople();
+});
+
+getPeople = () => {
+    $.ajax({
+        url: "http://localhost:3000/people",
+        success: result => {
+            $('#people').html("");
+
+            result.forEach(person => {
+                $("#people").append('<tr><td>' + person.name + '</td><td>' + person.age + '</td><td>' + person.phone + '</td><td>' + person.id + '</td></tr>');
+            });
+        }
+    });
+}
+
+sendInfo = ()=>{
+    let form = $('.formulario');
+    $.ajax({
+        url: "http://localhost:3000/people/add",
+        type:'POST',
+        data:form.serialize(),
+        success: result => getPeople()
+    });
 }
